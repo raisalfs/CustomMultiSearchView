77aa2409a2d216a93f93d8dbffc4e35a0173d148
raisalfs <isnooze404@gmail.com>
2021-05-07 22:18:25 +0700

    Adding some features

---
+++ multisearchviewlib/src/main/java/com/iammert/library/ui/multisearchviewlib/MultiSearchView.kt
@@ -0,0 +1,63 @@
+package com.iammert.library.ui.multisearchviewlib
+
+import android.content.Context
+import android.graphics.Color
+import android.os.Handler
+import android.os.Looper
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+import com.iammert.library.ui.multisearchviewlib.databinding.ViewMultiSearchBinding
+import com.iammert.library.ui.multisearchviewlib.extensions.inflate
+
+class MultiSearchView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+    RelativeLayout(context, attrs, defStyleAttr) {
+
+    interface MultiSearchViewListener {
+
+        fun onTextChanged(index: Int, s: CharSequence)
+
+        fun onSearchComplete(index: Int, s: CharSequence)
+
+        fun onSearchItemRemoved(index: Int)
+
+        fun onItemSelected(index: Int, s: CharSequence)
+
+        fun onSearchBehavior(isFirst: Boolean)
+
+        fun onSearchTabIsEmpty()
+    }
+    private val binding = inflate<ViewMultiSearchBinding>(R.layout.view_multi_search)
+    init {
+        val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.MultiSearchView, defStyleAttr, defStyleAttr)
+        val searchTextStyle = typedArray.getResourceId(R.styleable.MultiSearchView_searchTextStyle, 0)
+        val imageSource = typedArray.getResourceId(R.styleable.MultiSearchView_searchIcon, R.drawable.ic_round_search_24px)
+
+        binding.searchViewContainer.apply {
+            this.searchTextStyle = searchTextStyle
+        }
+
+        setSearchIconDrawable(imageSource)
+        binding.imageViewSearch.setOnClickListener {
+            if (binding.searchViewContainer.isInSearchMode().not()) {
+                binding.searchViewContainer.search()
+            } else {
+                binding.searchViewContainer.completeSearch()
+            }
+        }
+    }
+
+    fun setSearchViewListener(multiSearchViewListener: MultiSearchViewListener) {
+        binding.searchViewContainer.setSearchViewListener(multiSearchViewListener)
+    }
+    fun setSearchIconDrawable(drawable: Int) {
+        binding.imageViewSearch.setImageResource(drawable)
+    }
+    fun openSearchView() {
+        Handler(Looper.getMainLooper()).postDelayed({ // This method will be executed once the timer is over
+            binding.searchViewContainer.setOnClickListener {
+                binding.imageViewSearch.performClick()
+            }
+            binding.searchViewContainer.performClick()
+        }, 200)
+    }
+}
